c607e61fa678249f13e41b32ba205af17f3722c5
Ben Weitzman <benjamin.weitzman@tufts.edu>
2011-12-16 02:10:15 -0500

    implemented mega search (display only, need to redirect to appropriate page)

--- ajax/views.py
+++ ajax/views.py
@@ -5,18 +5,30 @@ from django.contrib.auth.models import User
 
 def index(request):
     message= "[]"
-    if request.is_ajax():
+    if request.is_ajax() or True:
         kind = request.GET['kind']
         letters = request.GET['letters']
         model = Artist
-        if kind == "Artist":
-            model = Artist
-            a = model.objects.values('name').filter(name__startswith=letters)
-        elif kind == "Album":
-            model = Album
-            a = model.objects.values('name').filter(name__startswith=letters)
-        elif kind == "User":
+        response = dict()
+        searchtypes = list()
+        if kind == "mega":
+            searchtypes = request.user.get_profile().searchtypes()
+        if kind == "artists" or 1 in searchtypes:
+            a = Artist.objects.values('name').filter(name__startswith=letters)
+            if a:
+                response['artists'] = list(a)
+        if kind == "albums" or 2 in searchtypes:
+            a = Album.objects.values('name').filter(name__startswith=letters)
+            if a:
+                response['albums'] = list(a)
+        if kind == "users" or 3 in searchtypes:
             a = User.objects.values('username').filter(username__startswith=letters)
-        if a:
-            message = simplejson.dumps(list(a))
+            if a:
+                response['users'] = list(a)
+        if len(response) != 0:
+            message = simplejson.dumps(response)
     return HttpResponse(message)
+
+#def mega(request):
+    #if request.is_ajax():
+        
