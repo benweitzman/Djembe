49b3fca5727cb170d7685660de0346fcebe756a9
benweitzman <benjamin.weitzman@tufts.edu>
2011-12-24 14:20:59 -0500

    added pages to forum, added posts per page option, added last viewed post in thread option

--- userprofile/views.py
+++ userprofile/views.py
@@ -3,25 +3,23 @@ from userprofile.models import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.db.models import Count
+from forum.models import *
 
 def mine(request):
-    showuser = request.user
+    showuser = User.objects.annotate(Count("post")).get(id=request.user.id)
     return render_to_response("profile/view.html",context_instance=RequestContext(request,{"showuser":showuser}))
 
 def view(request,username=""):
     if "username" in request.GET:
         return HttpResponseRedirect("/user/"+request.GET["username"])
-    showuser = User.objects.annotate(Count("posts")).get(username=username)
+    showuser = User.objects.annotate(Count("post")).get(username=username)
     return render_to_response("profile/view.html",context_instance=RequestContext(request,{"showuser":showuser}))
 
 def editProfile(request):
     if request.POST:
         user = User.objects.get(id=request.user.id)
         f = ProfileForm(request.POST,instance=user.get_profile())
-        """csi = ""
-        for i in dict(f.data)['megasearch']:
-            csi += i+","
-        f.data.__setitem__("megasearch",csi)"""
         if f.is_valid():
             u = f.save(commit=False)
             csi = ""
